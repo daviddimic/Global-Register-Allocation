bace348c08c60a6c2b82ffc32ea2746417d27db4
David Dimic <daviddimic@hotmail.com>
2018-07-11 22:10:44 +0200

    liveness analysis function

--- livenessAnalysis.py
+++ livenessAnalysis.py
@@ -1,29 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import basicBlock as bb
 import parser
-import yacc as yacc
+import yacc
 
 def livenessAnalysis(basicBlocks):
     bb.ReverseListOfBasicBlocks(basicBlocks)
     bb.PrintBasicBlocks(basicBlocks)
 
-    # for block in basicBlocks:
-    #     listOfInstructions = block.__getInstructions__()
-    #     for instr in listOfInstructions:
-    #         parseInstruction(instr)
-    instructionList = basicBlocks[0].__getInstructions__()
-    parseInstruction(instructionList[0])
-    #for l in instructionList:
-    #    parseInstruction(l)
+    for block in basicBlocks:
+        listOfInstructions = block.__getInstructions__()
+        use = set([])
+        for instr in listOfInstructions:
+            useI, killI = parseInstruction(instr)
+            use = use.union(set(useI))
+            use = use.difference(set(killI))
+            print(use)
 
 def parseInstruction(instruction):
-    instruction = ' '.join(instruction.rsplit()[1:]) # remove number of instruction
-    yacc.parse('ifFalse v >2 goto 4')
-    print(parser.use)
+    #remove number of instruction
+    instruction = ' '.join(instruction.rsplit()[1:])
+    yacc.parse(instruction)
+    return parser.use, parser.kill
 
 def main():
     fileName = 'testBasicBlocks/bbtest2.txt'
     basicBlocks = bb.CreateListOfBasicBlocksFromFile(fileName)
     livenessAnalysis(basicBlocks)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
