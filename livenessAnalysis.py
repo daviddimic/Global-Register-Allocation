d15a0303e92ef3224f99d0f8b4c74b8f2cd21dd3
David Dimic <daviddimic@hotmail.com>
2018-07-12 16:52:21 +0200

    adj list from liveness list

--- livenessAnalysis.py
+++ livenessAnalysis.py
@@ -46,11 +46,11 @@ def modelGraph(livenessList):
     graph = []
 
     for instr in livenessList:
-        if len(instr) == 1 and instr not in graph:
+        if len(instr) == 1 and [instr[0], instr[0]] not in graph:
             #don't add [a] if [a, _] or [_, a] already exists in graph
             used_nodes = reduce(lambda a,b: a + b, graph) if graph != [] else []
             if instr[0] not in used_nodes:
-                graph.append(instr)
+                graph.append([instr[0], instr[0]])
 
         for i, node1 in enumerate(instr):
             for node2 in instr[i+1:]:
@@ -59,10 +59,10 @@ def modelGraph(livenessList):
 
                 #if exist [a]
                 #remove [a] and later add [a, _] or [_, a]
-                if [node1] in graph:
-                    graph.remove([node1])
-                if [node2] in graph:
-                    graph.remove([node2])
+                if [node1, node1] in graph:
+                    graph.remove([node1, node1])
+                if [node2, node2] in graph:
+                    graph.remove([node2, node2])
 
                 #add [a, b] if [a, b] or [b, a] don't exist in graph
                 if t not in graph and t_rev not in graph:
